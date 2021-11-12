51eb87a93579fad4eb130528b2c10eded3a3f965
quenti20 <avikpat2000@gmail.com>
2021-11-12 17:26:51 +0530

    first comit

---
+++ first.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+class first{
+    public static void main(String[] args) {
+        int a , b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b= sc.nextInt();
+        int sum = a+b;
+        System.out.println("Sum is "+sum);
+    }
+}
